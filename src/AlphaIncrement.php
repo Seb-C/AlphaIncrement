cb4e22723b148ac68b0da3f36a1005f22b484825
Sébastien Caparros <sebastien-caparros.name>
2016-11-19 16:52:51 +0100

    Working implementation with unit tests

--- src/AlphaIncrement.php
+++ src/AlphaIncrement.php
@@ -4,21 +4,129 @@ namespace AlphaIncrement;
 
 class AlphaIncrement
 {
+    const DEFAULT_LENGTH = 5;
     const DEFAULT_ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const DEFAULT_SHUFFLE_ALPHABET = true;
+
     protected $length;
     protected $alphabet;
 
-    public function __construct($length = 5, $alphabet = self::DEFAULT_ALPHABET)
-    {
+    public function __construct(
+        $length          = self::DEFAULT_LENGTH,
+        $alphabet        = self::DEFAULT_ALPHABET,
+        $shuffleAlphabet = self::DEFAULT_SHUFFLE_ALPHABET
+    ) {
+        assert($length > 0);
+        assert(strlen($alphabet) > 0);
+
         $this->length = $length;
         $this->alphabet = $alphabet;
+
+        if ($shuffleAlphabet) {
+            $this->alphabet = $this->pseudoRandomShuffle($this->alphabet);
+        }
     }
 
     public function encode($integerId)
     {
         assert(is_int($integerId)); // PHP 5.6 : can't type the param as int :(
         assert($integerId >= 0);
+        
+        $newId = $this->convertPositiveIntegerToCustomBase($integerId);
+        $newId = $this->prefixWithCharacterRepresentingZero($newId);
+        $newId = $this->applyVigenereCipher($newId);
 
-        return 'Hello World!';
+        return $this->charIndexesToString($newId);
+    }
+
+    public function pseudoRandomShuffle($alphabet)
+    {
+        mt_srand(strlen($alphabet));
+        $alphabetAsArray = str_split($alphabet);
+        usort($alphabetAsArray, function ($char) {
+            return mt_rand(-1, 1);
+        });
+        return implode('', $alphabetAsArray);
+    }
+
+    /**
+     * Converts the given integer to the base defined (by radix)
+     * by the alphabet `$this->alphabet`.
+     * Returns the resulting string as an array of indexes in the alphabet.
+     */
+    public function convertPositiveIntegerToCustomBase($number)
+    {
+        $expectedLength = $this->getExpectedLengthInCustomBase($number);
+
+        $digits = [];
+        $remaining = $number;
+        for ($i = $expectedLength - 1; $i > 0; $i--) {
+            $positionValue = strlen($this->alphabet) ** $i;
+            $digits[] = (int) floor($remaining / $positionValue);
+            $remaining = $remaining % $positionValue;
+        }
+        $digits[] = $remaining;
+
+        return $digits;
+    }
+
+    public function getExpectedLengthInCustomBase($number)
+    {
+        $expectedLength = 0;
+        while(strlen($this->alphabet) ** ($expectedLength + 1) <= $number) {
+            $expectedLength++;
+        }
+        return $expectedLength + 1;
+    }
+
+    /**
+     * Converts an array of indexes to a string with the
+     * characters at those indexes in the alphabet.
+     */
+    public function charIndexesToString(array $indexes)
+    {
+        $string = '';
+        foreach ($indexes as $index) {
+            $string .= $this->alphabet[$index];
+        }
+        return $string;
+    }
+
+    /**
+     * Left pads the given string with the character representing
+     * zero in `$this->alphabet`, to match the expected length `$this->length`.
+     */
+    public function prefixWithCharacterRepresentingZero(array $indexes)
+    {
+        while(count($indexes) < $this->length) {
+            array_unshift($indexes, 0);
+        }
+        return $indexes;
+    }
+
+    /**
+     * Generates a pseudo random integer arrays, in a way
+     * that the last elements of the array are always
+     * the same for a given alphabet.
+     */
+    public function getCaesarCipherSwitchAmounts($length)
+    {
+        mt_srand(strlen($this->alphabet));
+        $switchs = [];
+        for ($i = 0; $i < $length; $i++) {
+            array_unshift($switchs, mt_rand());
+        }
+        return $switchs;
+    }
+
+    public function applyVigenereCipher(array $indexes)
+    {
+        $switchs = $this->getCaesarCipherSwitchAmounts(count($indexes));
+
+        $newIndexes = [];
+        for ($i = 0; $i < count($indexes); $i++) {
+            $newIndexes[] = ($indexes[$i] + $switchs[$i]) % strlen($this->alphabet);
+        }
+        return $newIndexes;
     }
 }
--- tests/AlphaIncrementTest.php
+++ tests/AlphaIncrementTest.php
@@ -5,30 +5,95 @@ use AlphaIncrement\AlphaIncrement;
 
 class AlphaIncrementTest extends TestCase
 {
-    protected $length;
-    protected $alphabet;
-    protected $alphaIncrement;
-
-    public function __construct()
+    public function testExpectedLengthInCustomBase()
     {
-        parent::__construct();
-        $this->length = 2;
-        $this->alphabet = 'AB1';
-        $this->alphaIncrement = new AlphaIncrement($this->length, $this->alphabet);
+        $ai = new AlphaIncrement(2, 'AB1', false);
+        $this->assertEquals(1, $ai->getExpectedLengthInCustomBase(0));
+        $this->assertEquals(1, $ai->getExpectedLengthInCustomBase(1));
+        $this->assertEquals(2, $ai->getExpectedLengthInCustomBase(3));
+        $this->assertEquals(2, $ai->getExpectedLengthInCustomBase(5));
+        $this->assertEquals(3, $ai->getExpectedLengthInCustomBase(10));
+        $this->assertEquals(3, $ai->getExpectedLengthInCustomBase(11));
+        $this->assertEquals(4, $ai->getExpectedLengthInCustomBase(28));
     }
 
-    public function testEncoding()
+    public function testConvertPositiveIntegerToCustomBase()
     {
-        $possibleCombinationsBeforeOverflow = strlen($this->alphabet) ** $this->length;
+        $ai = new AlphaIncrement(2, 'AB1', false);
+        $this->assertEquals([0],          $ai->convertPositiveIntegerToCustomBase(0));
+        $this->assertEquals([1],          $ai->convertPositiveIntegerToCustomBase(1));
+        $this->assertEquals([1, 0],       $ai->convertPositiveIntegerToCustomBase(3));
+        $this->assertEquals([1, 2],       $ai->convertPositiveIntegerToCustomBase(5));
+        $this->assertEquals([1, 0, 1],    $ai->convertPositiveIntegerToCustomBase(10));
+        $this->assertEquals([1, 0, 2],    $ai->convertPositiveIntegerToCustomBase(11));
+        $this->assertEquals([1, 1, 1],    $ai->convertPositiveIntegerToCustomBase(13));
+        $this->assertEquals([1, 0, 0, 1], $ai->convertPositiveIntegerToCustomBase(28));
+    }
+
+    public function testCharIndexesToString()
+    {
+        $ai = new AlphaIncrement(2, 'AB1', false);
+        $this->assertEquals('A',    $ai->charIndexesToString([0]));
+        $this->assertEquals('B',    $ai->charIndexesToString([1]));
+        $this->assertEquals('1',    $ai->charIndexesToString([2]));
+        $this->assertEquals('AB1',  $ai->charIndexesToString([0, 1, 2]));
+        $this->assertEquals('11AB', $ai->charIndexesToString([2, 2, 0, 1]));
+        $this->assertEquals('A1BB', $ai->charIndexesToString([0, 2, 1, 1]));
+    }
+
+    public function testPrefixWithCharacterRepresentingZero()
+    {
+        $ai = new AlphaIncrement(2, 'AB1', false);
+        $this->assertEquals([0, 0], $ai->prefixWithCharacterRepresentingZero([]));
+        $this->assertEquals([0, 1], $ai->prefixWithCharacterRepresentingZero([1]));
+        $this->assertEquals([0, 2], $ai->prefixWithCharacterRepresentingZero([2]));
+        $this->assertEquals([1, 2], $ai->prefixWithCharacterRepresentingZero([1, 2]));
+    }
+
+    public function testGetCaesarCipherSwitchAmounts()
+    {
+        $alphabet = 'ABCDEF';
+        $ai = new AlphaIncrement(5, $alphabet, false);
+        $sequence5 = $ai->getCaesarCipherSwitchAmounts(5);
+        $sequence10 = $ai->getCaesarCipherSwitchAmounts(10);
+
+        $this->assertCount(5, $sequence5);
+        $this->assertCount(10, $sequence10);
+        $this->assertSame($sequence5, array_slice($sequence10, -5));
+        foreach ($sequence10 as $index) {
+            $this->assertTrue(is_int($index));
+        }
+    }
+
+    public function testApplyVigenereCipher()
+    {
+        $alphabet = 'ABCDEF';
+        $ai = new AlphaIncrement(5, $alphabet, false);
+
+        $result = $ai->applyVigenereCipher([0, 0, 5, 1, 3]);
+        $this->assertCount(5, $result);
+        foreach ($result as $index) {
+            $this->assertLessThan(strlen($alphabet), $index);
+            $this->assertTrue($index >= 0);
+        }
+    }
+
+    public function testEncode()
+    {
+        $length = 2;
+        $alphabet = 'AB1';
+        $ai = new AlphaIncrement($length, $alphabet, false);
+
+        $possibleCombinationsBeforeOverflow = strlen($alphabet) ** $length;
         $existingAlphaIds = [];
         for ($numericId = 0; $numericId < $possibleCombinationsBeforeOverflow * 3; $numericId++) {
-            $alphaId = $this->alphaIncrement->encode($numericId);
+            $alphaId = $ai->encode($numericId);
             if ($numericId < $possibleCombinationsBeforeOverflow) {
                 // Testing the cases where the id is too long to fit into the specified length when converted as a string.
                 // In that case, it is expected to overflow the specified limit.
-                $this->assertEquals(strlen($alphaId), $this->length, "The alpha id `$alphaId` doesn't match the expected length ($this->length).");
+                $this->assertEquals(strlen($alphaId), $length, "The alpha id `$alphaId` doesn't match the expected length ($length).");
             }
-            $this->assertRegExp("/^[$this->alphabet]+$/", $alphaId, "The alpha id `$alphaId` doesn't match the expected alphabet ($this->alphabet).");
+            $this->assertRegExp("/^[$alphabet]+$/", $alphaId, "The alpha id `$alphaId` doesn't match the expected alphabet ($alphabet).");
             $this->assertFalse(in_array($alphaId, $existingAlphaIds), "The alpha id `$alphaId` has been generated multiple times from a different numeric id ($numericId).");
             $existingAlphaIds[] = $alphaId;
         }
